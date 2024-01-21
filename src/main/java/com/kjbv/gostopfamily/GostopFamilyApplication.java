b1a3c4caf573175cacfca8a168fa83ad793d834f
kimjongbuem <kimjongbuem@github.com>
2024-01-21 10:45:26 +0900

    initalizing first

---
+++ src/main/java/com/kjbv/gostopfamily/GostopFamilyApplication.java
@@ -0,0 +1,13 @@
+package com.kjbv.gostopfamily;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GostopFamilyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GostopFamilyApplication.class, args);
+	}
+
+}
